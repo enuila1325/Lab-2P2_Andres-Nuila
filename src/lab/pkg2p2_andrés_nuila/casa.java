7ba7d6c2edd7b9fa10a7cfe420e0f6a141ec75d7
Andres Nuila <enuila634@gmail.com>
2020-05-08 13:25:28 -0600

    Inicio de proyecto

---
+++ src/lab/pkg2p2_andrés_nuila/casa.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab.pkg2p2_andrés_nuila;
+
+/**
+ *
+ * @author enuil
+ */
+public class casa {
+    
+}
